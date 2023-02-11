36fb7b0ed25cd52da0d0495ae4857023e75f429c
kishankushwaha8866664200 <84864817+kishankushwaha8866664200@users.noreply.github.com>
2023-02-11 15:45:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var song = "";
+var song1 = "";
+function preload() {
+   song = loadSound("music.mp3");
+   song1 = loadSound("music2.mp3");
+}
+function setup() {
+    canvas = createCanvas(540, 540);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 540, 540);
+}
